3aca710acd3eeacb5c9ff0b2a275ebae73fff7dc
cat-erina <56516698+cat-erina@users.noreply.github.com>
2019-12-22 16:39:43 +0100

    Update func_1.py

--- func_1.py
+++ func_1.py
@@ -1,14 +1,14 @@
 # note: set ourD=0 and visited=[starting_node] when calling the function func_1
 def func_1(G, visited, starting_node, maxD, ourD):
-    if ourD<=maxD:
-        for a in G[starting_node]:
-            if a not in visited:
+    if ourD<=maxD:     #we haven't reached the threshold distance
+        for a in G[starting_node]:   #look at every a adjacent of the starting node
+            if a not in visited:     
                 #print('a ',a)
                 #print('visited ',visited)
-                w=int(G.get_edge_data(starting_node,a)['weight'])
+                w=int(G.get_edge_data(starting_node,a)['weight']) #we add the weight of the edge to a to our distance count
                 #print('ourD+w ',ourD+w)
-                if ourD+w<=maxD:
+                if ourD+w<=maxD:     #if a is reachable with a distance <= threshold
                     ourD=ourD+w
-                    visited.append(a)
-                    func_1(G, visited, a, maxD, ourD)
+                    visited.append(a) . #we add a to the reachable and visited nodes
+                    func_1(G, visited, a, maxD, ourD) #recursively calling the function on a 
     return visited
